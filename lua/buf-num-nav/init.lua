5ab234d03e09eb42493caa8c1a57a4c17035b0d3
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-15 15:30:44 +0100

    Fix buflist to exclude all diagnostic message

--- lua/buf-num-nav/init.lua
+++ lua/buf-num-nav/init.lua
@@ -1,36 +1,52 @@
 local opts = {
 	leader = "<c-g>",
-    create_buffer = "c",
-    remove_buffer = "q",
-    -- Enable this binding in all modes by default
-    supported_modes = { "n", "v", "i", "o", "c", "t" }
+	create_buffer = "c",
+	remove_buffer = "q",
+	-- Enable this binding in all modes by default
+	supported_modes = { "n", "v", "i", "o", "c", "t" },
 }
 
 local function jump_to_buf(buf_num)
-    buf_num = tonumber(buf_num)
-    local list = vim.api.nvim_list_bufs()
-    print('check buf list', vim.inspect(list))
-    if buf_num > #list then
-        return
-    end
-    vim.api.nvim_set_current_buf(list[buf_num])
+	buf_num = tonumber(buf_num)
+
+	local list = vim.tbl_map(function(v)
+		return v.bufnr
+	end, vim.fn.getbufinfo({ buflisted = 1 }))
+
+	if buf_num > #list then
+		return
+	end
+
+	vim.api.nvim_set_current_buf(list[buf_num])
 end
 
 local function setup(user_opts)
 	opts = vim.tbl_extend("force", opts, user_opts or {})
-    -- To wipe out ephemeral buffer without disturbe the cyclelist
-    -- https://github.com/neovim/neovim/issues/13429
-    -- vim.o.bufhidden = "wipe"
-    for i=1,9 do
-        for _, mode in ipairs(opts.supported_modes) do
-            vim.api.nvim_set_keymap(mode, opts.leader .. i, "<cmd>lua require('buf-num-nav').jump_to_buf('" .. i .. "')<cr>", { silent = true, noremap = true })
-			vim.api.nvim_set_keymap(mode, opts.leader .. opts.create_buffer, ":edit! ", { silent = false, noremap = true })
-			vim.api.nvim_set_keymap(mode, opts.leader .. opts.remove_buffer, "<cmd>bdelete<cr>", { silent = true, noremap = true })
-        end
-    end
+	for i = 1, 9 do
+		for _, mode in ipairs(opts.supported_modes) do
+			vim.api.nvim_set_keymap(
+				mode,
+				opts.leader .. i,
+				"<cmd>lua require('buf-num-nav').jump_to_buf('" .. i .. "')<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.api.nvim_set_keymap(
+				mode,
+				opts.leader .. opts.create_buffer,
+				":edit! ",
+				{ silent = false, noremap = true }
+			)
+			vim.api.nvim_set_keymap(
+				mode,
+				opts.leader .. opts.remove_buffer,
+				"<cmd>bdelete<cr>",
+				{ silent = true, noremap = true }
+			)
+		end
+	end
 end
 
 return {
-    setup = setup,
-    jump_to_buf = jump_to_buf
+	setup = setup,
+	jump_to_buf = jump_to_buf,
 }
